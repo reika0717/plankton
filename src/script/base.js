332d9ea4f0b1c75ce47aa3be55c908a1a8fc8ece
Reika Hirahara <hirahara@Akios-MacBook-Pro-Retina-15-inch-Mid-2014.local>
2018-03-29 08:50:15 +0900

    implement top-page

--- src/script/base.js
+++ src/script/base.js
@@ -1,19 +1,21 @@
 $(function(){
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
+  // $("#header").load("header.html");
+  // $("#footer").load("footer.html");
 
-  var navi = $('header').offset().top;
-  console.log(navi)
-  $(window).scroll(function(){
-    if($(this).scrollTop() >= navi){
-      $('header').addClass('fixed')
-    } else {
-      $('header').removeClass('fixed')
-    }
-  });
+  // var navi = $('header').offset().top;
+  // console.log(navi)
+  // $(window).scroll(function(){
+  //   if($(this).scrollTop() >= navi){
+  //     $('header').addClass('fixed')
+  //   } else {
+  //     $('header').removeClass('fixed')
+  //   }
+  // });
 
-  $(document).on('click', 'li.nav__list-parent', function () {
+  $('li.nav__list-parent').hover(function () {
     var list_child = $(this).find('ul')
     $(list_child).slideToggle()
   })
+
+
 });
