6b4869e9f68ee2a8df7faef596ee04a23f3eff43
Reika Hirahara <hirahara@Akios-MacBook-Pro-Retina-15-inch-Mid-2014.local>
2018-04-20 14:21:23 +0900

    implement smooth scroll

--- src/script/base.js
+++ src/script/base.js
@@ -1,17 +1,44 @@
-$(function() {
-  $('.scroll-btn').on('click', function(e) {
+$(function () {
+  $('.scroll-btn').on('click', function (e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
   });
 
   $('#footer').load('../src/footer.html')
 
-  $('a[href^="#"]').click(function() {
-    var speed = 500;
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if (urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash);
+    }, 100);
+  }
+
+  //通常のクリック時
+  $('a[href^="#"]').click(function () {
+    //ページ内リンク先を取得
     var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
     return false;
   });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({
+      scrollTop: position
+    }, 500);
+  }
 });
